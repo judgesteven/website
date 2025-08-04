179b61c01c781f3026811591564760e43057a56d
Steven Judge <stevenjudge@Stevens-MacBook-Pro.local>
2025-08-04 07:23:56 +0300

    Restore original footer layout with logo on left side

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,29 +13,9 @@ const Footer = () => {
     <footer className="bg-gray-900 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Main content */}
-        <div className="flex flex-col space-y-6">
-          {/* Top row - Logo, Company info, and Social icons */}
-          <div className="flex flex-col lg:flex-row justify-between items-center space-y-4 lg:space-y-0">
-            {/* Left side - Logo */}
-            <div className="flex items-center">
-              <img 
-                src="/GameLayer_Logo_White.png" 
-                alt="GameLayer Logo" 
-                className="h-8 w-auto"
-              />
-            </div>
-            
-            {/* Center - Company info */}
-            <div className="text-center">
-              <p className="text-gray-400 text-sm">
-                Company ID - 2503589-1
-              </p>
-              <p className="text-gray-400 text-sm mt-1">
-                Copyright 2012 - 2025 GameLayer Oy - all rights reserved
-              </p>
-            </div>
-            
-            {/* Right side - Social icons */}
+        <div className="flex flex-col space-y-4">
+          {/* Top row - Social icons */}
+          <div className="flex justify-center lg:justify-end">
             <div className="flex space-x-4">
               {socialLinks.map((social) => (
                 <a
@@ -50,9 +30,29 @@ const Footer = () => {
             </div>
           </div>
           
-          {/* Bottom row - Legal links */}
-          <div className="flex justify-center">
-            <div className="flex space-x-6">
+          {/* Bottom row - Logo, Copyright and Legal links */}
+          <div className="flex flex-col lg:flex-row justify-between items-center space-y-2 lg:space-y-0">
+            {/* Left side - Logo */}
+            <div className="flex items-center">
+              <img 
+                src="/GameLayer_Logo_White.png" 
+                alt="GameLayer Logo" 
+                className="h-8 w-auto"
+              />
+            </div>
+            
+            {/* Center - Company info */}
+            <div className="text-center lg:text-left">
+              <p className="text-gray-400 text-sm">
+                Company ID - 2503589-1
+              </p>
+              <p className="text-gray-400 text-sm mt-1">
+                Copyright 2012 - 2025 GameLayer Oy - all rights reserved
+              </p>
+            </div>
+            
+            {/* Right side - Legal links */}
+            <div className="flex flex-col items-center lg:items-end space-y-1">
               <Link
                 to="/privacy"
                 className="text-gray-400 hover:text-white transition-colors duration-200 text-sm"
